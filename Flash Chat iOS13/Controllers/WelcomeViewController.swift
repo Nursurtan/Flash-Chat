5b13539ad348617ccbab5834e81aaab3b1417b8e
Nursurtan <arapovnur23@gmail.com>
2022-03-01 23:10:41 +0700

    Added Firebase

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -15,6 +15,8 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+       
+        
         titleLabel.text = ""
         var charIndex = 0.0
         let titleText = "⚡️FlashChat"
